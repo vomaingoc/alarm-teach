82bd04cd278a7ecf585d70c45ab38f9f1dbfdfb4
Tran Tien Dat <trantiendat016@gmail.com>
2016-11-03 23:53:47 +0700

    Reminder page

---
+++ app/src/main/java/com/example/alarm_crash/Alarm.java
@@ -0,0 +1,229 @@
+/**************************************************************************
+ *
+ * Copyright (C) 2012-2015 Alex Taradov <alex@taradov.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************************************************************************/
+
+package com.example.alarm_crash;
+
+import java.lang.System;
+import java.lang.Comparable;
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Calendar;
+
+import android.content.Intent;
+import android.content.Context;
+
+public class Alarm implements Comparable<Alarm>
+{
+  private Context context;
+  private long id;
+  private String title;
+  private long date;
+  private boolean enabled;
+  private int occurence;
+  private int days;
+  private long nextOccurence;
+
+  public static final int ONCE = 0;
+  public static final int WEEKLY = 1;
+
+  public static final int NEVER = 0;
+  public static final int EVERY_DAY = 0x7f;
+
+  public Alarm(Context context)
+  {
+    this.context = context;
+    id = 0;
+    title = "";
+    date = System.currentTimeMillis();
+    enabled = true;
+    occurence = ONCE;
+    days = EVERY_DAY;
+    update();
+  }
+
+  public long getId()
+  {
+    return id;
+  }
+
+  public void setId(long id)
+  {
+    this.id = id;
+  }
+
+  public String getTitle()
+  {
+    return title;
+  }
+
+  public void setTitle(String title)
+  {
+    this.title = title;
+  }
+
+  public int getOccurence()
+  {
+    return occurence;
+  }
+
+  public void setOccurence(int occurence)
+  {
+    this.occurence = occurence;
+    update();
+  }
+
+  public long getDate()
+  {
+    return date;
+  }
+
+  public void setDate(long date)
+  {
+    this.date = date;
+    update();
+  }
+
+  public boolean getEnabled()
+  {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled)
+  {
+    this.enabled = enabled;
+  }
+
+  public int getDays()
+  {
+    return days;
+  }
+
+  public void setDays(int days)
+  {
+    this.days = days;
+    update();
+  }
+
+  public long getNextOccurence()
+  {
+    return nextOccurence;
+  }
+
+  public boolean getOutdated()
+  {
+    return nextOccurence < System.currentTimeMillis();
+  }
+
+  public int compareTo(Alarm aThat)
+  {
+    final long thisNext = getNextOccurence();
+    final long thatNext = aThat.getNextOccurence();
+    final int BEFORE = -1;
+    final int EQUAL = 0;
+    final int AFTER = 1;
+
+    if (this == aThat)
+      return EQUAL;
+
+    if (thisNext > thatNext)
+      return AFTER;
+    else if (thisNext < thatNext)
+      return BEFORE;
+    else
+      return EQUAL;
+  }
+
+  public void update()
+  {
+    Calendar now = Calendar.getInstance();
+
+    if (occurence == WEEKLY)
+    {
+      Calendar alarm = Calendar.getInstance();
+
+      alarm.setTimeInMillis(date);
+      alarm.set(now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
+
+      if (days != NEVER)
+      {
+        while (true)
+        {
+          int day = (alarm.get(Calendar.DAY_OF_WEEK) + 5) % 7;  
+
+          if (alarm.getTimeInMillis() > now.getTimeInMillis() && (days & (1 << day)) > 0)
+            break;
+
+          alarm.add(Calendar.DAY_OF_MONTH, 1);
+        }
+      }
+      else
+      {
+        alarm.add(Calendar.YEAR, 10);
+      }
+
+      nextOccurence = alarm.getTimeInMillis();
+    }
+
+    date = nextOccurence;
+  }
+
+  public void toIntent(Intent intent)
+  {
+    intent.putExtra("com.taradov.alarmme.id", id);
+    intent.putExtra("com.taradov.alarmme.title", title);
+    intent.putExtra("com.taradov.alarmme.date", date);
+    intent.putExtra("com.taradov.alarmme.alarm", enabled);
+    intent.putExtra("com.taradov.alarmme.occurence", occurence);
+    intent.putExtra("com.taradov.alarmme.days", days);
+  }
+
+  public void fromIntent(Intent intent)
+  {
+    id = intent.getLongExtra("com.taradov.alarmme.id", 0);
+    title = intent.getStringExtra("com.taradov.alarmme.title");
+    date = intent.getLongExtra("com.taradov.alarmme.date", 0);
+    enabled = intent.getBooleanExtra("com.taradov.alarmme.alarm", true);
+    occurence = intent.getIntExtra("com.taradov.alarmme.occurence", 0);
+    days = intent.getIntExtra("com.taradov.alarmme.days", 0);
+    update();
+  }
+
+  public void serialize(DataOutputStream dos) throws IOException
+  {
+    dos.writeLong(id);
+    dos.writeUTF(title);
+    dos.writeLong(date);
+    dos.writeBoolean(enabled);
+    dos.writeInt(occurence);
+    dos.writeInt(days);
+  }
+ 
+  public void deserialize(DataInputStream dis) throws IOException
+  {
+    id = dis.readLong();
+    title = dis.readUTF();
+    date = dis.readLong();
+    enabled= dis.readBoolean();
+    occurence= dis.readInt();
+    days = dis.readInt();
+    update();
+  }
+}
+
---
+++ app/src/main/java/com/example/alarm_crash/MainActivity.java
@@ -0,0 +1,218 @@
+package com.example.alarm_crash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "AlarmCrash";
+
+    private ListView mAlarmList;
+    private AlarmListAdapter mAlarmListAdapter;
+    private Alarm mCurrentAlarm;
+
+    private final int NEW_ALARM_ACTIVITY = 0;
+    private final int EDIT_ALARM_ACTIVITY = 1;
+    private final int PREFERENCES_ACTIVITY = 2;
+    private final int ABOUT_ACTIVITY = 3;
+
+    private final int CONTEXT_MENU_EDIT = 0;
+    private final int CONTEXT_MENU_DELETE = 1;
+    private final int CONTEXT_MENU_DUPLICATE = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddAlarmClick(view);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ImageButton btnSetting = (ImageButton) findViewById(R.id.imageButton);
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(v.getContext(), "click setting", Toast.LENGTH_SHORT);
+            }
+        });
+
+        TextView textCurrentTime = (TextView) findViewById(R.id.textCurrentTime);
+        textCurrentTime.setText((new SimpleDateFormat("HH:mm")).format(new Date()));
+
+        TextView textCurrentDay = (TextView) findViewById(R.id.textCurrentDay);
+        textCurrentDay.setText((new SimpleDateFormat("ddd, MMM dd, yyyy")).format(new Date()));
+
+        mAlarmList = (ListView)findViewById(R.id.alarm_list);
+
+        mAlarmListAdapter = new AlarmListAdapter(this);
+        mAlarmList.setAdapter(mAlarmListAdapter);
+        mAlarmList.setOnItemClickListener(mListOnItemClickListener);
+        registerForContextMenu(mAlarmList);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        Log.i(TAG, "AlarmMe.onResume()");
+        mAlarmListAdapter.updateAlarms();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == NEW_ALARM_ACTIVITY)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                mCurrentAlarm.fromIntent(data);
+                mAlarmListAdapter.add(mCurrentAlarm);
+            }
+            mCurrentAlarm = null;
+        }
+        else if (requestCode == EDIT_ALARM_ACTIVITY)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                mCurrentAlarm.fromIntent(data);
+                mAlarmListAdapter.update(mCurrentAlarm);
+            }
+            mCurrentAlarm = null;
+        }
+        else if (requestCode == PREFERENCES_ACTIVITY)
+        {
+            mAlarmListAdapter.onSettingsUpdated();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (R.id.menu_settings == item.getItemId())
+        {
+            Intent intent = new Intent(getBaseContext(), Preferences.class);
+            startActivityForResult(intent, PREFERENCES_ACTIVITY);
+            return true;
+        }
+        else if (R.id.menu_about == item.getItemId())
+        {
+            Intent intent = new Intent(getBaseContext(), About.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        if (v.getId() == R.id.alarm_list)
+        {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+
+            menu.setHeaderTitle(mAlarmListAdapter.getItem(info.position).getTitle());
+            menu.add(Menu.NONE, CONTEXT_MENU_EDIT, Menu.NONE, "Edit");
+            menu.add(Menu.NONE, CONTEXT_MENU_DELETE, Menu.NONE, "Delete");
+            menu.add(Menu.NONE, CONTEXT_MENU_DUPLICATE, Menu.NONE, "Duplicate");
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        int index = item.getItemId();
+
+        if (index == CONTEXT_MENU_EDIT)
+        {
+            Intent intent = new Intent(getBaseContext(), EditAlarm.class);
+
+            mCurrentAlarm = mAlarmListAdapter.getItem(info.position);
+            mCurrentAlarm.toIntent(intent);
+            startActivityForResult(intent, EDIT_ALARM_ACTIVITY);
+        }
+        else if (index == CONTEXT_MENU_DELETE)
+        {
+            mAlarmListAdapter.delete(info.position);
+        }
+        else if (index == CONTEXT_MENU_DUPLICATE)
+        {
+            Alarm alarm = mAlarmListAdapter.getItem(info.position);
+            Alarm newAlarm = new Alarm(this);
+            Intent intent = new Intent();
+
+            alarm.toIntent(intent);
+            newAlarm.fromIntent(intent);
+            newAlarm.setTitle(alarm.getTitle() + " (copy)");
+            mAlarmListAdapter.add(newAlarm);
+        }
+
+        return true;
+    }
+
+    public void onAddAlarmClick(View view)
+    {
+        Intent intent = new Intent(getBaseContext(), EditAlarm.class);
+
+        mCurrentAlarm = new Alarm(this);
+        mCurrentAlarm.toIntent(intent);
+
+        startActivityForResult(intent, NEW_ALARM_ACTIVITY);
+    }
+
+    private AdapterView.OnItemClickListener mListOnItemClickListener = new AdapterView.OnItemClickListener()
+    {
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            Intent intent = new Intent(getBaseContext(), EditAlarm.class);
+
+            mCurrentAlarm = mAlarmListAdapter.getItem(position);
+            mCurrentAlarm.toIntent(intent);
+            startActivityForResult(intent, EDIT_ALARM_ACTIVITY);
+        }
+    };
+}
